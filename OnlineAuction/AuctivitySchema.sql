6586e79862415ff916526f22c179a0e9b21afe09
ksporwal <kunalprwl59@gmail.com>
2020-10-03 22:22:21 +0530

    Updated sql table queries

--- OnlineAuction/AuctivitySchema.sql
+++ OnlineAuction/AuctivitySchema.sql
@@ -11,7 +11,7 @@
 --                          6) User.wallet is an optional entity and is not set to "NOT NULL";
 --                          7) Primary key of respective tables have been provided at the end of each table;
 
-create table OnlineAuctionDB.User (
+create table OnlineAuctionDB.Usertable (
 	UserID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 100, INCREMENT BY 1),
     Name varchar(255) NOT NULL,
     dob varchar(100) NOT NULL,
@@ -22,7 +22,7 @@ create table OnlineAuctionDB.User (
     address varchar(255) NOT NULL,
     user_type int NOT NULL,
     wallet int,
-    PRIMARY KEY (Personid)
+    PRIMARY KEY (UserID)
 );
 
 create table OnlineAuctionDB.Category (
@@ -40,26 +40,25 @@ create table OnlineAuctionDB.Product (
 	ActualPrice INTEGER NOT NULL,
 	Quantity INTEGER NOT NULL,
 	Image varchar(255) NOT NULL,
-	SellerID INTEGER NOT NULL references OnlineAuctionDB.User(UserID),
+	SellerID INTEGER NOT NULL references OnlineAuctionDB.Usertable(UserID),
 	PRIMARY KEY(ProductID)
 );
 
 create table OnlineAuctionDB.Bid (
 	BidID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 400, INCREMENT BY 1),
-	BidderID INTEGER NOT NULL references OnlineAuctionDB.User(UserID),
+	BidderID INTEGER NOT NULL references OnlineAuctionDB.Usertable(UserID),
 	BidProductID INTEGER NOT NULL references OnlineAuctionDB.Product(ProductID),
 	BidValue INTEGER NOT NULL,
-	Status INTEGER NOT NULL,
+	Status INTEGER NOT NULL
 	--Status has 3 values : 1 -> Bid is Open; 2 -> Bid is Lost; 3 -> Bid is Won.
 );
-
-create table OlineAuctionDB.ProductBid (
+create table OnlineAuctionDB.ProductBid (
 	MinBidValue INTEGER NOT NULL,
 	BidStartDate Date,
 	BidEndDate Date,
-	BuyerID INTEGER NOT NULL references OnlineAuctionDB.User(UserID),
+	BuyerID INTEGER NOT NULL references OnlineAuctionDB.Usertable(UserID),
 	SoldPrice INTEGER NOT NULL,
-	Status INTEGER NOT NULL,
+	Status INTEGER NOT NULL
 	-- Status has 4 values : 1 -> New Product for bid;
 	--                       2 -> Product bid is completed and unsold
 	--                       3 -> Product bid is completed and sold;
