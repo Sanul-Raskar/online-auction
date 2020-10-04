da27b09619780603d493dbc8cf8e269bf1e0d87a
Sanul Raskar <sanulraskar@gmail.com>
2020-10-04 19:44:35 +0530

    load products on home page

--- OnlineAuction/WebContent/resources/js/addProducts.js
+++ OnlineAuction/WebContent/resources/js/addProducts.js
@@ -1,72 +1,3 @@
-/*validateProductName function is check if product name is not null or empty.
- * If product name is valid then return true else false */
-function validateProductName(productName) {
-	if (productName !== null && productName !== "" && productName !== " ") {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-/*
- * validateProductDescription function is check if product description is not
- * null or empty. If product description is valid then return true else false
- */
-function validateProductDescription(productName) {
-	if (productName !== null && productName !== "" && productName !== " ") {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-/*
- * validateActualPrice function is check if price is not null or empty. If price
- * is valid then return true else false
- */
-function validateActualPrice(actualPrice) {
-	/*
-	 * Regular expression pattern to check if price is decimal number. Only upto
-	 * 3 digits are allowed after decimal point
-	 */
-	let pattern = /^\d+(\.\d{1,3})?$/;
-	if (actualPrice !== null && actualPrice !== "" && actualPrice !== " "
-			&& pattern.test(actualPrice)) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-/*
- * validateUploadImage function is check if price is not null or empty. If price
- * is valid then return true else false
- */
-function validateUploadImage() {
-	var imageInput = document.getElementById('img');
-	var imagePath = imageInput.value;
-
-	// allowed file types
-	var imgExtensions = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
-
-	if (!imgExtensions.exec(imagePath)) {
-		alert('Invalid Image Extension');
-		imageInput.value = '';
-		return false;
-	} else {
-		// Image preview
-		if (imageInput.files && imageInput.img[0]) {
-			var reader = new FileReader();
-			reader.onload = function(e) {
-				document.getElementById('imagePreview').innerHTML = '<img src="'
-						+ e.target.result + '"/>';
-			};
-
-			reader.readAsDataURL(imageInput.img[0]);
-		}
-	}
-}
-
 function validateAddProductForm(event) {
 	// Prevent form from automatic submit
 	event.preventDefault()
@@ -105,12 +36,12 @@ function validateAddProductForm(event) {
 		document.getElementById("quantityError").innerHTML = "Please enter product quantity";
 	}
 
-	if (validateUploadImage()) {
-		formData.append("image", addProductForm["img"].files[0]);
-	} else {
-		validData = false;
-		document.getElementById("imageError").innerHTML = "Accepted image formats are jpg, jpeg, png and gif";
-	}
+//	if (validateUploadImage()) {
+//		formData.append("image", addProductForm["img"].files[0]);
+//	} else {
+//		validData = false;
+//		document.getElementById("imageError").innerHTML = "Accepted image formats are jpg, jpeg, png and gif";
+//	}
 
 	if (validData) {
 		console.log(formData);
@@ -119,8 +50,7 @@ function validateAddProductForm(event) {
 		http.open('POST', "./addProduct", true);
 
 		// Send the proper header information along with the request
-		http.setRequestHeader('Content-type',
-				'multipart/form-data');
+		http.setRequestHeader('Content-type', 'multipart/form-data');
 
 		// Call a function when the state changes.
 		http.onreadystatechange = function() {
---
+++ OnlineAuction/WebContent/resources/js/home.js
@@ -0,0 +1,86 @@
+function loadProducts() {
+    var productsArr = [
+      {
+        productId: 1,
+        productName: "Product 1",
+        actualPrice: 56,
+        minBidAmount: 70,
+        productImg: "./resources/img/logo.jpg",
+        startTime: "Sun, 04 Oct 2020 07:14:47 GMT",
+        endTime: "Sun, 04 Oct 2020 07:14:47 GMT",
+      },
+      {
+        productId: 2,
+        productName: "Product 2",
+        actualPrice: 56,
+        minBidAmount: 70,
+        productImg: "./resources/img/logo.jpg",
+        startTime: "Sun, 04 Oct 2020 07:14:47 GMT",
+        endTime: "Sun, 04 Oct 2020 07:14:47 GMT",
+      },
+      {
+        productId: 3,
+        productName: "Product 3",
+        actualPrice: 56,
+        minBidAmount: 70,
+        productImg: "./resources/img/logo.jpg",
+        startTime: "Sun, 04 Oct 2020 07:14:47 GMT",
+        endTime: "Sun, 04 Oct 2020 07:14:47 GMT",
+      },
+      {
+        productId: 4,
+        productName: "Product 4",
+        actualPrice: 56,
+        minBidAmount: 70,
+        productImg: "./resources/img/logo.jpg",
+        startTime: "Sun, 04 Oct 2020 07:14:47 GMT",
+        endTime: "Sun, 04 Oct 2020 07:14:47 GMT",
+      },
+      {
+        productId: 5,
+        productName: "Product 5",
+        actualPrice: 56,
+        minBidAmount: 70,
+        productImg: "./resources/img/logo.jpg",
+        startTime: "Sun, 04 Oct 2020 07:14:47 GMT",
+        endTime: "Sun, 04 Oct 2020 07:14:47 GMT",
+      },
+      {
+        productId: 6,
+        productName: "Product 6",
+        actualPrice: 56,
+        minBidAmount: 70,
+        productImg: "./resources/img/logo.jpg",
+        startTime: "Sun, 04 Oct 2020 07:14:47 GMT",
+        endTime: "Sun, 04 Oct 2020 07:14:47 GMT",
+      },
+      {
+        productId: 7,
+        productName: "Product 7",
+        actualPrice: 56,
+        minBidAmount: 70,
+        productImg: "./resources/img/logo.jpg",
+        startTime: "Sun, 04 Oct 2020 07:14:47 GMT",
+        endTime: "Sun, 04 Oct 2020 07:14:47 GMT",
+      },
+    ];
+
+    productsArr.forEach((product) => {
+      let div = document.createElement("div");
+      div.id = product.productId;
+      div.className = "card";
+      div.innerHTML = `
+    	  <img src=${product.productImg} class="productImg">
+    	  <br/>
+    	  <h2 class="text-center">${product.productName}</h2>
+    	  <p><span style="font-weight:bold">Actual Price:</span>&nbsp;Rs. ${product.actualPrice}</p>
+    	  <p> <span style="font-weight:bold">Min Bid Amount:</span>&nbsp;Rs. ${product.minBidAmount}</p>
+    	  <p><span style="font-weight:bold">Bit Start Time:</span> ${product.startTime}</p>
+    	  <p><span style="font-weight:bold">Bit Start Time:</span> ${product.endTime}</p>
+    	  <br/>
+      `;
+      document.getElementById("productsContainer").appendChild(div);
+    });
+  }
+
+  loadProducts();
