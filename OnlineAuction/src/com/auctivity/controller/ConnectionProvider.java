7ca96affd020ee672cb83b06c339b20748ed72ed
jayeshc2810 <31742015+jayeshc2810@users.noreply.github.com>
2020-10-04 17:44:09 +0530

    Modified getConnection

---
+++ OnlineAuction/src/com/auctivity/controller/ConnectionProvider.java
@@ -0,0 +1,48 @@
+/**
+ * This java file is class that returns connection parameter 
+ * after establishing connection with database
+ */
+package com.auctivity.controller;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionProvider {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public static Connection getConnection() throws SQLException {
+		
+		Connection con = null;
+		String url = null;
+		String driver = null;
+		String username = null;
+		String password = null;
+		
+		try {
+			Properties properties = new Properties();
+			FileInputStream fis = new FileInputStream("/online-auction-model/OnlineAuction/WebContent/WEB-INF/properties/connection.properties");
+			properties.load(fis);
+			driver = properties.getProperty(driver);
+			url = properties.getProperty(url);
+			username = properties.getProperty(username);
+			password = properties.getProperty(password);
+			
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, username, password);
+			
+		} catch (ClassNotFoundException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return con;
+		
+	}
+}
